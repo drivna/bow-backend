e625a0d1973e99a2f94e0d14d28951f5dbc249d5
mohtashimkamran <mohtashimValyx>
2025-08-27 13:56:50 +0530

    Updated

--- app/routes/chat_routes.py
+++ app/routes/chat_routes.py
@@ -17,7 +17,7 @@ from app.utils.chat_util import send_reply_to_user
 chat_api_ns = Namespace("chats", description="APIs for Chats")
 
 
-@chat_api_ns.route("/")
+@chat_api_ns.route("/message")
 class ChatRoute(Resource):
     parser: RequestParser = RequestParser()
     parser.add_argument("message", help="Message content", required=True)
