24f916ed670098f3c0bd1cc21fc7ee9cd5e88143
mohtashimkamran <mohtashimValyx>
2025-08-31 16:43:41 +0530

    Store reply to user

--- app/utils/chat_util.py
+++ app/utils/chat_util.py
@@ -1,4 +1,6 @@
 from typing import Optional
+
+from loguru import logger
 from app.constants import ChatGptMessagePayload, ChatGptPrompts
 from app.database.models.chats import ChatModel
 from app.database.models.file_chats import FileChatModel
@@ -7,7 +9,7 @@ from app.utils.c_gpt import fetch_and_stream_response_from_model
 
 
 def send_reply_to_user(
-    user_message: str, file_content: str, user_id: str, chat_id: str, file_id: Optional[str] = None
+    user_message: str, file_content: str, user_id: str, chat_id: str, file_id: str
 ):
     prompt: str = ChatGptPrompts.get_chat_prompt(
         user_message=user_message, file_content=file_content
@@ -23,8 +25,8 @@ def send_reply_to_user(
     new_chat = ChatModel(user_id=user_id, message=reply_for_user, is_from_system=True)
     ObjectRepository.insert_single_object(new_chat)
 
-    if file_id is not None:
-        file_chat_entry = FileChatModel(user_id=user_id, chat_id=chat_id, file_id=file_id)
-        ObjectRepository.insert_single_object(file_chat_entry)
+    file_chat_entry = FileChatModel(user_id=user_id, chat_id=chat_id, file_id=file_id)
+    ObjectRepository.insert_single_object(file_chat_entry)
+    logger.info(f"Upsert fone for filechat: {file_chat_entry.id}")
 
     return
