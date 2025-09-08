fdeadf1c19aa9c5d985277a49913acfd5fa1d9b6
mohtashimkamran <mohtashimValyx>
2025-09-08 22:11:14 +0530

    port to env

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ OPEN_AI_API_KEY: str = os.getenv("CHAT_GPT_API_KEY", "")
 GEMINI_API_KEY: str = os.getenv("CELERY_BROKER_URL", "")
 CELERY_RESULT_BACKEND: str = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
 CUSTOM_USER_ID: str = "user_17ae337cff"
+APP_PORT: int = os.getenv("APP_PORT", "4000")
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from flask_cors import CORS
 
+from app.config import APP_PORT
 from app.main import app
 
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
+    app.run(host="0.0.0.0", port=APP_PORT, debug=True)
